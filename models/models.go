096bb20ec107186c97c75a776d1d1b68d83ab054
AshRoy29 <ash.rox2920@gmail.com>
2021-12-20 23:02:45 +0600

    initial commit

--- models/models.go
+++ models/models.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Appointment struct {
-	ID        primitive.ObjectID `json:"_id" bson:"_id"`
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	FirstName string             `json:"first_name"`
 	LastName  string             `json:"last_name"`
 	Phone     string             `json:"phone"`
@@ -16,4 +16,54 @@ type Appointment struct {
 	Message   string             `json:"message"`
 	CreatedAt time.Time          `json:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at"`
+}
+
+type Specialties struct {
+	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name string             `json:"name"`
+}
+
+type Doctors struct {
+	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	FirstName   string             `json:"first_name"`
+	LastName    string             `json:"last_name"`
+	Phone       string             `json:"phone"`
+	Email       string             `json:"email"`
+	Specialties Specialties        `json:"specialties"`
+	Degrees     []string           `json:"degrees"`
+	Description string             `json:"description"`
+	Awards      []string           `json:"awards"`
+	Image       string             `json:"image"`
+	//CreatedAt time.Time `json:"created_at"`
+	//UpdatedAt time.Time `json:"updated_at"`
+}
+
+type User struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	FirstName string             `json:"first_name"`
+	LastName  string             `json:"last_name"`
+	Phone     string             `json:"phone"`
+	Email     string             `json:"email"`
+	Password  string             `json:"password"`
+	//Prescriptions []string `json:"prescriptions"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Authentication struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Token struct {
+	Role        string `json:"role"`
+	Email       string `json:"email"`
+	TokenString string `json:"token"`
+}
+
+type Role struct {
+	Admin  string `json:"admin"`
+	Doctor string `json:"doctor"`
+	User   string `json:"user"`
 }
