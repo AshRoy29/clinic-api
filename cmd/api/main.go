0425b9297b869834cc6966e332f84f594589932d
AshRoy29 <ash.rox2920@gmail.com>
2021-12-22 23:05:11 +0600

    initial commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	r := Routes()
+	fmt.Println("Server is getting started...")
+	http.ListenAndServe(":4000", r)
+	fmt.Println("Listening at port 4000...")
+}
