ca29b10e452a7e45dab96c4d6cd0ee164423a171
AshRoy29 <ash.rox2920@gmail.com>
2021-12-24 03:54:29 +0600

    initial commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,65 @@
 package main
 
 import (
-	"fmt"
+	. "clinic-api/config"
+	"github.com/gorilla/handlers"
+	"log"
 	"net/http"
 )
 
+//type config struct {
+//	port int
+//	env string
+//	db struct{
+//		dsn string
+//	}
+//	jwt struct{
+//		secret string
+//	}
+//}
+//
+//type AppStatus struct {
+//	Status string `json:"status"`
+//	Environment string `json:"environment"`
+//	Version string `json:"version"`
+//}
+//
+//type application struct {
+//	config config
+//	logger *log.Logger
+//	models models.Models
+//}
+
+var config Config
+
+func init() {
+	config.Read()
+}
+
+//func initFlags() {
+//	config.Server.Port = flag.Lookup("Port").Value.(flag.Getter).Get().(string)
+//}
+
 func main() {
-	r := Routes()
-	fmt.Println("Server is getting started...")
-	http.ListenAndServe(":4000", r)
-	fmt.Println("Listening at port 4000...")
+
+	//flag.Parse()
+	//initFlags()
+
+	//logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	//srv := &http.Server{
+	//	Addr:         fmt.Sprintf(config.Server.Port),
+	//	Handler:      Routes(),
+	//	IdleTimeout:  time.Minute,
+	//	ReadTimeout:  10 * time.Second,
+	//	WriteTimeout: 30 * time.Second,
+	//}
+
+	headersOK := handlers.AllowedHeaders([]string{"X-Requested-With", "Access-Control-Allow-Origin", "Content-Type", "Authorization"})
+	methodsOK := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"})
+	originOK := handlers.AllowedOrigins([]string{"*"})
+
+	log.Println("Starting server on port", config.Server.Port)
+
+	log.Fatal(http.ListenAndServe(config.Server.Port, handlers.CORS(headersOK, methodsOK, originOK)(Routes())))
 }
