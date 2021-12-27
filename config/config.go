d51af8c808668aba7288fc0831e652622881c7bb
AshRoy29 <ash.rox2920@gmail.com>
2021-12-28 02:39:36 +0600

    initial commit

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ func (c *Config) Read() {
 	c.Database.DatabaseName = "clinic"
 	c.Database.Username = ""
 	c.Database.Password = ""
-	c.Jwt.Secret = "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160"
+	//c.Jwt.Secret = "secretkeyjwt"
 	//flag.StringVar(&c.Server.Port, "Server", ":4000", "Server Port")
 	////flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
 	//flag.StringVar(&c.Database.Uri, "Uri", "mongodb://localhost:27017", "MongoDB connection string")
@@ -36,4 +36,5 @@ func (c *Config) Read() {
 	//flag.StringVar(&c.Database.Username, "Username", "", "MongoDB username string")
 	//flag.StringVar(&c.Database.Password, "Password", "", "MongoDB password string")
 	//flag.StringVar(&c.Jwt.Secret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "secret")
+
 }
