3facbea8eedc0d716f57826afec18a3916e6b46f
AshRoy29 <ash.rox2920@gmail.com>
2021-12-28 02:40:35 +0600

    initial commit

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,12 +4,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt/v4"
 	"log"
 	"net/http"
 )
-
-//var config Config
 
 func IsAuthorized(handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -20,20 +18,39 @@ func IsAuthorized(handler http.HandlerFunc) http.HandlerFunc {
 		}
 		//secretkey := "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160"
 
-		var mySigningKey = []byte(config.Jwt.Secret)
+		tokenString := r.Header["Token"][0]
+		log.Println(r.Header["Token"][0])
 
-		token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+		mySigningKey := []byte("secret")
+
+		log.Println(mySigningKey)
+
+		log.Println(config.Jwt.Secret)
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+			log.Println(token.Method.Alg())
+			log.Println(token.Method.(*jwt.SigningMethodHMAC))
+
+			log.Println(token.Method)
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				log.Println("error in parsing")
 				return nil, fmt.Errorf("error in parsing")
 			}
+			log.Println("OK")
 			return mySigningKey, nil
 		})
 
 		if err != nil {
-			err := errors.New("your token has expired")
-			json.NewEncoder(w).Encode(err)
-			return
+			log.Println(err)
+			//err := errors.New("your token has expired")
+			//json.NewEncoder(w).Encode(err)
+			//return
 		}
+
+		log.Println(token.Claims.(jwt.MapClaims))
+		log.Println(token)
+		log.Println(token.Valid)
 
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 			log.Println("vbnm")
@@ -50,22 +67,6 @@ func IsAuthorized(handler http.HandlerFunc) http.HandlerFunc {
 		errs := errors.New("your token has expired")
 		json.NewEncoder(w).Encode(errs)
 		json.NewEncoder(w).Encode(err)
-	}
-}
 
-func AdminIndex(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Role") != "admin" {
-		w.Write([]byte("Not authorized."))
-		return
 	}
-	w.Write([]byte("Welcome, Admin."))
-}
-
-func UserIndex(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Role") != "user" {
-		w.Write([]byte("Not Authorized."))
-		return
-	}
-
-	w.Write([]byte("Welcome, User."))
 }
